e533534d32d6a9c8173744ba06fee35f815f7e68
n0tmanu <156202625+n0tmanu@users.noreply.github.com>
2024-05-17 01:45:33 +0530

    minor fixes

--- media/admin.py
+++ media/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Folder, File
 
+
+class FolderAdmin(admin.ModelAdmin):
+    search_fields = ['id', 'name']  # Add fields you want to search here
+
+
 admin.site.register(Folder)
 admin.site.register(File)
